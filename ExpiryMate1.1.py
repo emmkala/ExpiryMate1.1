123517efc09936e39621dc71d05c56863e0b5141
Rylen Sampson <16ras6@queensu.ca>
2018-02-03 21:54:29 -0500

    Here is my code

---
+++ ExpiryMate1.1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+##Create home page for the UI
+homePage = Tk()
+size = Label(width=1920 ,height=1200)
+size.pack()
+
+test_1 = Label(homePage, text="Test")
+test_1.grid(row=0)
+
+homePage.mainloop()
